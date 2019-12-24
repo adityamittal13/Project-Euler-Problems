e81273db21f8b46a58cb3900e8f1a9a70ffc2209
adityamittal13 <39227064+adityamittal13@users.noreply.github.com>
2019-12-23 19:12:24 -0800

    Update Fibbonaci.py

--- Fibbonaci.py
+++ Fibbonaci.py
@@ -1,18 +1,10 @@
-arr = []
+sum1 = 0;
 
-n = 4000000
-i = 0
-
-n0 = 1
-n1 = 1
-
-sum1 = 0
-
-while (i < n):
-	i = n0 + n1
-	if (i % 2 == 0):
+for i in range(1000):
+	if (i % 3 == 0):
 		sum1 += i
-	n0 = n1
-	n1 = i
+	elif (i % 5 == 0):
+		sum1 += i
 
-print(sum1)
+
+print (sum1)
